5820c49fdcd2ea9c026e5b2fbb60e22791270916
muhammad2503 <abdullaevmuhammadyusuf2@gmail.com>
2022-04-15 08:11:07 +0500

    footer2

---
+++ my-react-17/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      Footer
+      <h1>Footerni ishlatish</h1>
+      <h1>Footerni 2-marta</h1>
+    </div>
+  );
+};
+
+export default Footer;

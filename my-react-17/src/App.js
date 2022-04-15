db36bf20440f9bdede7f954da3f590c9fcfd09c7
bek-exadel <ukhusanov@exadel.com>
2022-04-15 07:41:32 +0500

    react hello

--- my-react-17/src/App.js
+++ my-react-17/src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>hello world!</h1>
+      <h1>hello react!</h1>
+      <p>hdbdhdbf hdfhbbfb</p>
     </div>
   );
 }
